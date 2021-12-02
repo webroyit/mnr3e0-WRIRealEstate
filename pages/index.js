732a82a9be8786a4ffca4f2438084895ae811847
royweb <xytank@gmail.com>
2021-12-02 13:31:44 -0500

    Add rapid api to fetch real estate from Bayut API

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';   // This is an image coming from next.js
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+
 const Banner = ({ purpose, title1, title2, description1, description2, buttonText, linkName, imageUrl  }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} />
@@ -12,7 +14,7 @@ const Banner = ({ purpose, title1, title2, description1, description2, buttonTex
       <Text fontSize="3xl" fontWeight="bold">
         {title1}<br />{title2}
       </Text>
-      <Text color="gray7500" fontSize="lg" paddingTop="3" paddingBottom="3" fontWeight="medium">
+      <Text color="gray.700" fontSize="lg" paddingTop="3" paddingBottom="3" fontWeight="medium">
         {description1}<br />{description2}
       </Text>
       <Button fontSize='xl' bg="blue.300">
@@ -22,12 +24,10 @@ const Banner = ({ purpose, title1, title2, description1, description2, buttonTex
   </Flex>
 );
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
+  console.log( propertiesForSale, propertiesForRent);
   return (
-    <div>
-      <h1>
-        WRI Real Esate
-      </h1>
+    <Box>
       <Banner
         purpose={'RENT A HOME'}
         title1="Rental Homes for"
@@ -37,6 +37,9 @@ export default function Home() {
         buttonText='Explore Renting'
         linkName="/search?purpose=for-rent"
         imageUrl="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" />
+      <Flex flexWrap="wrap">
+
+      </Flex>
       <Banner
         purpose={'BUY A HOME'}
         title1="Find, Buy, & Own Your"
@@ -46,6 +49,22 @@ export default function Home() {
         buttonText='Explore Buying'
         linkName="/search?purpose=for-sale"
         imageUrl="https://images.unsplash.com/photo-1575517111478-7f6afd0973db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
-    </div>
+      <Flex flexWrap="wrap">
+      
+      </Flex>
+    </Box>
   )
+}
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    // Next.js wil adds these to the props of the component
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits
+    }
+  }
 }
