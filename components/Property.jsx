5d206495a7927e4a537b322bdeddfb5567aac738
royweb <xytank@gmail.com>
2021-12-08 13:58:54 -0500

    Use millify to format large numbers

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,6 +5,7 @@ import { Avatar } from '@chakra-ui/avatar';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
+import millify from 'millify';
 
 import DefaultImage from '../assets/images/defaultHouse.jpg';
 
@@ -20,14 +21,14 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
                         <Box paddingRight="3" color="green.400">
                             {isVerified && <GoVerified />}
                         </Box>
-                        <Text fontWeight="bold" fontSize="lg">AED {price}{rentFrequency && `/${rentFrequency}`}</Text>
+                        <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
                     </Flex>
                     <Box>
                         <Avatar size="sm" src={agency?.logo?.url} />
                     </Box>
                 </Flex>
                 <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
-                    {rooms} <FaBed /> | {baths} <FaBath /> | {area} sqft <BsGridFill />
+                    {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
                 </Flex>
                 <Text fontSize="lg">
                     {title.length > 30 ? `${title.substring(0, 30)}...` : title}
