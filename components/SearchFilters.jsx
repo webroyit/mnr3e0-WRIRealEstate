512ee5dd1f3ef98c89f95e37f8b87f4072f226db
royweb <xytank@gmail.com>
2021-12-21 15:41:39 -0500

    Add Search Filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,11 +4,33 @@ import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
+import { filterData, getFilterValues } from '../utils/filterData';
+
 const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+
+    const searchProperties = (filterValues) => {
+        console.log(filterValues);
+    }
+
     return (
-        <div>
-            search
-        </div>
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}>
+                            {filter?.items?.map((item) => (
+                                <option key={item.value} value={item.value}>
+                                    {item.name}
+                                </option>
+                            ))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
     )
 }
 
