e0c67b1607cbe925f17778baccf17903cb70fc86
royweb <xytank@gmail.com>
2021-12-21 14:08:50 -0500

    Fetch real estate data from API using getServerSideProps()

--- pages/search.js
+++ pages/search.js
@@ -6,9 +6,10 @@ import { BsFilter } from "react-icons/bs";
 
 import SearchFilters from "../components/SearchFilters";
 import Property from "../components/Property";
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 import noresult from '../assets/images/noresult.svg'
 
-const Search = () => {
+const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -34,9 +35,9 @@ const Search = () => {
                 Properties {router.query.purpose}
             </Text>
             <Flex flexWrap="wrap">
-                {[].map((property) => <Property property={property} key={property.id} />)}
+                {properties.map((property) => <Property property={property} key={property.id} />)}
             </Flex>
-            {[].length === 0 && (
+            {properties.length === 0 && (
                 <Flex justifyContent="center" alignItems="center" flexDir="column" marginTop="5" marginBottom="5">
                     <Image src={noresult} alt="no result" />
                     <Text fontSize="2xl" marginTop="3">No Result Found.</Text>
@@ -47,3 +48,26 @@ const Search = () => {
 }
 
 export default Search;
+
+// Get query from URL
+export async function getServerSideProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+  
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);  
+  
+    return {
+        // Next.js wil adds these to the props of the component
+        props: {
+            properties: data?.hits
+        }
+    }
+}
