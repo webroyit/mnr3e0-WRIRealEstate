5947de6884bfa96e42186de77635c4cfdae04f53
royweb <xytank@gmail.com>
2021-12-24 11:48:47 -0500

    Add function to update URL based on filter option

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
-import { useRouter } from 'next/router';
+import router, { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
@@ -10,7 +10,17 @@ const SearchFilters = () => {
     const [filters, setFilters] = useState(filterData);
 
     const searchProperties = (filterValues) => {
-        console.log(filterValues);
+        const path = router.pathname;
+        const { query } = router;
+
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value
+        })
+
+        // Update current URL
+        router.push({ pathname: path, query });
     }
 
     return (
