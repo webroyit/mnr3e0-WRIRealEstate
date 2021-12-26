6797ede2c0b386447b72f82664331d6a7f91e1cc
royweb <xytank@gmail.com>
2021-12-26 16:08:50 -0500

    Add NProgress spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false });
+
+  // Show loading when the page changed
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+
+  // Stop loading when the page finish loading
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  })
+
   return (
     <>
+      <Head>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
+      </Head>
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />
